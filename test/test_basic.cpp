98fcce0c62966e74bebed74c69294c5eeea9e53b
Susan Long <dlsubjam@gmail.com>
2016-06-01 21:29:19 +0800

    Update test_basic.cpp

--- test/test_basic.cpp
+++ test/test_basic.cpp
@@ -1,5 +1,7 @@
 #include "R-Tree.hpp"
+#include <iostream>
 #include <cstdio>
+#include <algorithm>
 
 int main()
 {
@@ -7,17 +9,17 @@ int main()
 	for (int i = 0; i < 100; i++)
 	{
 		double f = i;
-		rt.insert({ 0,0,0,f,f,f});
+		rt.insert({0,0,0,f,f,f});
 	}
 	for (int i = 10; i < 60; i++)
 	{
 		double f = i;
-		rt.remove({ 0,0,0,f,f,f });
+		rt.remove({0,0,0,f,f,f});
 	}
 	for (int i = 0; i < 100; i++)
 	{
 		double f = i;
-		printf("%d\n", rt.search({ f,f,f,100,100,100 }));
+		printf("%d\n", rt.search({f,f,f,100,100,100}));
 	}
-
+	return 0;
 }

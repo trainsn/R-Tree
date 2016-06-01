863566fbdef8904b26cc29e5dc08615e208cbcfd
张哲恺 <sxtyzhangzk@live.cn>
2016-06-01 21:08:57 +0800

    Simple Test

---
+++ test/test_basic.cpp
@@ -0,0 +1,23 @@
+#include "R-Tree.hpp"
+#include <cstdio>
+
+int main()
+{
+	RTree rt;
+	for (int i = 0; i < 100; i++)
+	{
+		double f = i;
+		rt.insert({ 0,0,0,f,f,f});
+	}
+	for (int i = 10; i < 60; i++)
+	{
+		double f = i;
+		rt.remove({ 0,0,0,f,f,f });
+	}
+	for (int i = 0; i < 100; i++)
+	{
+		double f = i;
+		printf("%d\n", rt.search({ f,f,f,100,100,100 }));
+	}
+
+}
